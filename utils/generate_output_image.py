e9f9e212af27c12ee1a62347cbeaa6a95a8d3b85
Ronin Sharma <rrs234@cornell.edu>
2022-11-14 03:55:16 -0500

    some people claim that python is not real
    after seeing this commit, they'll pull an alex and eat a banana peel

--- utils/generate_output_image.py
+++ utils/generate_output_image.py
@@ -1,17 +1,46 @@
 from PIL import Image
 import numpy as np
+import sys
 
-output_array = []
 
-with open('output.txt', 'r') as f:
-    output_array = f.readlines()[0].strip()
+def load_data_from_file(file_name):
+    with open(file_name, 'r') as f:
+        input_data = f.readlines()[0].strip()
+    
+    return input_data
 
-print(output_array)
+def process_data(data, num_columns = 112): # CHANGE DEFAULT VALUE WHEN NEW DATA IS READY
+    output_array = []
+    row = []
+    for value in data:
+        new_value = int(value) * 255
+        if (len(row) < num_columns):
+            row.append(new_value)
+        else:
+            output_array.append(row)
+            row = [new_value]
+    output_array.append(row)
 
-#output_array = np.array(output_array)
+    return np.asarray( output_array, dtype = 'uint8' )
 
-#image = Image.fromarray(output_array)
-#image.save('output.jpg')
+def create_image(image_array, file_name):
+    image = Image.fromarray(image_array)
+    image.save(file_name)
+
+def main():
+    # Retrieve optional arguments
+    num_arguments = len(sys.argv)
+    input_file_name = sys.argv[1] if (num_arguments == 2) else 'output.txt'
+    output_image_name = sys.argv[2] if (num_arguments == 3) else 'output.png'
+
+    # Load input data
+    data = load_data_from_file(input_file_name)
+
+    # Process the data
+    processed_data = process_data(data)
+
+    # Create the output image
+    create_image(processed_data, output_image_name)
 
 if __name__ == '__main__':
     main()
