5b65f676c59e4b7f87d10a54802d64c5b7a0cf43
Ronin Sharma <rrs234@cornell.edu>
2022-11-16 00:32:41 -0500

    added setup script
    if you want to look at it you will need to decrypt
    
    it

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LC_ALL=en_US.UTF-8
+
+unset LM_LICENSE_FILE
+export XILINXD_LICENSE_FILE=2100@flex.ece.cornell.edu
+
+# Xilinx Vivado System Edition 2016.2
+source /opt/xilinx/xilinx_2016.2/Vivado/2016.2/settings64.sh
+
+git config --global alias.got '!git pull && echo "billy mode" > billy.txt && git add billy.txt && git commit -m "i love billy" -m "no, i am not being silly" && clear'
