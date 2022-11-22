891a77565495a2860e45f1c961c6ad4466c1b454
Ronin Sharma <rrs234@cornell.edu>
2022-11-21 23:40:22 -0500

    updated setup script
    the lakers would have more wins if yuno miles headlined at the crypt

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ export XILINXD_LICENSE_FILE=2100@flex.ece.cornell.edu
 # Xilinx Vivado System Edition 2016.2
 source /opt/xilinx/xilinx_2016.2/Vivado/2016.2/settings64.sh
 
-git config --global alias.got '!git pull && echo "billy mode" > billy.txt && git add billy.txt && git commit -m "i love billy" -m "no, i am not being silly" && clear'
+git config --global alias.got '!git pull && echo "billy mode" > billy.txt && git add billy.txt && git commit -m "i love billy" -m "no, i am not being silly" && git push && clear'
